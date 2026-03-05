2987c3b3399a2fff820a10ca0a2e0de6caad998a
kartaliesport-beep <kartaliesport@gmail.com>
2026-03-06 00:41:29 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:40:41.852Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T21:41:28.596Z
+Commit: Resolve null pointer exception in user service

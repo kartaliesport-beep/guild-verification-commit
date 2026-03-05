71bf2210ac5c999cd5747d5318a7e12e1ec69c72
kartaliesport-beep <kartaliesport@gmail.com>
2026-03-06 00:41:46 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:41:28.596Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T21:41:45.727Z
+Commit: Add multi-language support

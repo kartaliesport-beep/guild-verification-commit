c519581dd9182cbc8e3eb272d42eef928a138a5c
kartaliesport-beep <kartaliesport@gmail.com>
2026-03-06 00:44:12 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:44:09.127Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T21:44:11.587Z
+Commit: Resolve null pointer exception in user service

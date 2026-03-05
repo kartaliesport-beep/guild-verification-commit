c5a459f0be89ae041151ab88ad43a5dc0d28588f
kartaliesport-beep <kartaliesport@gmail.com>
2026-03-06 00:41:30 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:40:40.374Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T21:41:30.038Z
+Commit: Resolve timezone issues in date handling

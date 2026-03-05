b65b66d0960af1294ec4a087e28b7ca5dddb8065
kartaliesport-beep <kartaliesport@gmail.com>
2026-03-06 00:41:32 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:40:53.260Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T21:41:31.502Z
+Commit: Resolve null pointer exception in user service

a1d93cbfc1d72f3bf48dbcf5b9c39dbfad06e623
kartaliesport-beep <kartaliesport@gmail.com>
2026-03-06 00:44:22 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:44:14.933Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T21:44:21.673Z
+Commit: Fix broken links in documentation

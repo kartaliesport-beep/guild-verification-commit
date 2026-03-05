b8a95cf4bc10927d98c533008fbf01e54652d5fc
kartaliesport-beep <kartaliesport@gmail.com>
2026-03-06 00:43:59 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:43:54.791Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T21:43:58.535Z
+Commit: Patch API endpoint error handling
